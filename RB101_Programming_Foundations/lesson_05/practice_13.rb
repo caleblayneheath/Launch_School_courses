fea256818545c4e1f934832892cac3e0e73d18dd
Caleb Heath <caleb.heath@gmail.com>
2019-09-05 12:50:20 -0400

    found solution to 13

--- RB101_Programming_Foundations/lesson_05/practice_13.rb
+++ RB101_Programming_Foundations/lesson_05/practice_13.rb
@@ -2,22 +2,8 @@ arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
 p arr.sort
 # [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
 
-
-arr1 = arr.map do |sub_array|
-  # sub_array.sort_by { sub_array.select { |i| i.odd?}. sort }
-
-  # sub_array.sort_by do 
-  #   p sub_array.map do |elem|
-  #     elem = nil if elem.even?
-  #   end
-  # end
-
-  sub_array.sort_by do |a, b|
-    # a.select(&:odd?) <=> b.select(&:odd?)
-  end
-
+arr1 = arr.sort do |a, b|
+  a.select(&:odd?) <=> b.select(&:odd?)
 end
 
 p arr1
-
-# [[1, 7], [1, 9], [1, 3]]
