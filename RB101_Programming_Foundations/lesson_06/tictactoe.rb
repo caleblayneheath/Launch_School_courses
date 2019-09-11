f85eb1bd1b6226a31083a54348dc81ddcd770462
Caleb Heath <caleb.heath@gmail.com>
2019-09-11 16:49:57 -0400

    finish tictactoe bonus

--- RB101_Programming_Foundations/lesson_06/tictactoe.rb
+++ RB101_Programming_Foundations/lesson_06/tictactoe.rb
@@ -1,12 +1,14 @@
-require 'pry'
-require 'pry-byebug'
-
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                 [[1, 5, 9], [3, 5, 7]]
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+PLAYER_NAME = 'Player'
+COMPUTER_NAME = 'Computer'
+CHOOSE = 'choose'
+# options for FIRST_PLAYER are PLAYER_NAME, COMPUTER_NAME, and CHOOSE
+FIRST_PLAYER = CHOOSE
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -30,10 +32,12 @@ def initalize_board
   new_board
 end
 
+# returns array of integers expressing valid moves
 def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+# returns string of integer square choices for player
 def joinor(array, delimiter = ', ', word = 'or')
   if array.size >= 2
     [array[0..-2].join(delimiter), array[-1]].join("#{delimiter}#{word} ")
@@ -53,6 +57,8 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+# computer prioritizes offense, defense, center control
+# randomly selects valid move otherwise
 def computer_places_piece!(brd)
   attack_square = priority_square(brd, 'attack')
   defend_square = priority_square(brd, 'defend')
@@ -69,6 +75,8 @@ def computer_places_piece!(brd)
   brd[square] = COMPUTER_MARKER
 end
 
+# if computer 'attack', finds line it has 2 marks in but player has none
+# if computer 'defend', finds line player has 2 marks in but computer has none
 def priority_square(brd, mode)
   marker = COMPUTER_MARKER if mode == 'attack'
   marker = PLAYER_MARKER if mode == 'defend'
@@ -94,12 +102,13 @@ def someone_won?(brd)
   !!detect_winner(brd)
 end
 
+# if line is full all of one player's marks, return string as winner
 def detect_winner(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(*line).all?(PLAYER_MARKER)
-      return 'Player'
+      return PLAYER_NAME
     elsif brd.values_at(*line).all?(COMPUTER_MARKER)
-      return 'Computer'
+      return COMPUTER_NAME
     end
   end
   nil
@@ -111,25 +120,66 @@ def display_wins(score_hsh)
   prompt(result.join(' | '))
 end
 
+# returns name of winner as string
 def get_overall_winner(score_hsh)
   result = ''
   score_hsh.each { |name, wins| result = name if wins >= 5 }
   result
 end
 
+def place_piece!(brd, current_player)
+  if current_player == PLAYER_NAME
+    player_places_piece!(brd)
+  elsif current_player == COMPUTER_NAME
+    computer_places_piece!(brd)
+  end
+end
+
+def who_goes_first
+  case FIRST_PLAYER
+  when PLAYER_NAME
+    PLAYER_NAME
+  when COMPUTER_NAME
+    COMPUTER_NAME
+  when CHOOSE
+    choose_player
+  end
+end
+
+def choose_player
+  loop do
+    prompt("Who goes first? Player or computer?")
+    prompt("Type 'P' for player or 'C' for computer.")
+    input = gets.chomp.downcase[0]
+    if input == 'p'
+      return PLAYER_NAME
+    elsif input == 'c'
+      return COMPUTER_NAME
+    else
+      prompt('Invalid entry.')
+    end
+  end
+end
+
+def alternate_player(current_player)
+  case current_player
+  when PLAYER_NAME
+    COMPUTER_NAME
+  when COMPUTER_NAME
+    PLAYER_NAME
+  end
+end
+
 loop do
-  score = { 'Player' => 0, 'Computer' => 0 }
+  score = { PLAYER_NAME => 0, COMPUTER_NAME => 0 }
 
   loop do
     board = initalize_board
-
+    current_player = who_goes_first
     loop do
       display_board(board)
-
-      player_places_piece!(board)
-      break if someone_won?(board) || board_full?(board)
-
-      computer_places_piece!(board)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
       break if someone_won?(board) || board_full?(board)
     end
 
